a5d01a133cf18ba3d1fdd311fba81d2670628f1a
Stoabrogga <5532995-Stoabrogga@users.noreply.gitlab.com>
2020-04-30 00:28:10 +0200

    Remove conf.sh.dist

--- include.sh
+++ include.sh
@@ -2,8 +2,6 @@
 
 MOD_AH_BOT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/" && pwd )"
 
-source $MOD_AH_BOT_ROOT"/conf/conf.sh.dist"
-
-if [ -f $MOD_AH_BOT_ROOT"/conf/conf.sh" ]; then
-    source $MOD_AH_BOT_ROOT"/conf/conf.sh"
-fi
+DB_WORLD_CUSTOM_PATHS+=(
+        $MOD_AH_BOT_ROOT"/sql/world/base/"
+)
